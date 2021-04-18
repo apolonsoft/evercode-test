d318228f0d9d2d81ff302d4f8fe1a1cf8efc3db5
ahmad <apolonsoft@gmail.com>
2021-04-18 20:01:03 +0430

    docker files

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-./wait-for-it.sh $PSQL_HOST:$PSQL_PORT --timeout=60 --strict
+./wait-for-it.sh localhost:5432 --timeout=60 --strict
 
 if [ $? -ne 0  ]; then
   echo "PostgreSQL service is not starting up."
